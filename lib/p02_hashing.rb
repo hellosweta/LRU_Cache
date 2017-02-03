88c02e43dfcba4efed66eb145c6aa773ea9d2fac
App Academy Student <student@appacademy.io>
2017-02-03 11:45:36 -0800

    create hashing functions

--- lib/p02_hashing.rb
+++ lib/p02_hashing.rb
@@ -4,11 +4,21 @@ end
 
 class Array
   def hash
+    integer = 0
+    self.each_with_index do |el, idx|
+      integer += (el.ord * idx)
+    end
+    integer.hash
   end
 end
 
 class String
   def hash
+    integer = 0
+    self.each_char.with_index do |el, idx|
+      integer += (el.ord * idx)
+    end
+    integer.hash
   end
 end
 
@@ -16,6 +26,10 @@ class Hash
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+    integer = 0
+    self.each do |key, value|
+      integer += (key.to_s.ord * value.ord)
+    end
+    integer.hash
   end
 end
